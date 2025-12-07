f3809e6405674047983e351bbc4377de78b0dc46
GEOVANE ZOMER <geovane.zomer@gmail.com>
2025-12-07 00:34:06 +0000

    test: adicionar config_views.xml

--- gz_finance_docs/__manifest__.py
+++ gz_finance_docs/__manifest__.py
@@ -41,7 +41,7 @@ This module is fully open source under the GPL-2 license.
         # 'sale_offer',  # Removido - módulo customizado não padrão
     ],
     'data': [
-        # Teste 3: Security + Data + project_views
+        # Teste 4: + config_views
         'security/document_hub_security.xml',
         'security/ir.model.access.csv',
         'data/document_sequence.xml',
@@ -49,6 +49,7 @@ This module is fully open source under the GPL-2 license.
         'data/tags_data.xml',
         'data/cron_data.xml',
         'views/project_views.xml',
+        'views/config_views.xml',
     ],
     'assets': {
         'web.assets_backend': [
