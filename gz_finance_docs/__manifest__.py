a2689b47c72bcef1a380ea56ba8b96609866ab6e
GEOVANE ZOMER <geovane.zomer@gmail.com>
2025-12-07 00:38:36 +0000

    test: adicionar menu_views.xml - MANIFEST COMPLETO

--- gz_finance_docs/__manifest__.py
+++ gz_finance_docs/__manifest__.py
@@ -41,7 +41,7 @@ This module is fully open source under the GPL-2 license.
         # 'sale_offer',  # Removido - módulo customizado não padrão
     ],
     'data': [
-        # Teste 6: + res_config_settings_views
+        # Teste 7: COMPLETO - todos os arquivos
         'security/document_hub_security.xml',
         'security/ir.model.access.csv',
         'data/document_sequence.xml',
@@ -52,6 +52,7 @@ This module is fully open source under the GPL-2 license.
         'views/config_views.xml',
         'views/document_hub_views.xml',
         'views/res_config_settings_views.xml',
+        'views/menu_views.xml',
     ],
     'assets': {
         'web.assets_backend': [
